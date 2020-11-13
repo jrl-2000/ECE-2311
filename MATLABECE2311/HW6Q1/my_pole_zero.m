c39104ca4faff4359760d608f4ed5cd217e73ee0
Jonathan Lopez <jonathan.lopez.1224@gmail.com>
2020-11-13 12:44:03 -0500

    Add files via upload

--- MATLABECE2311/HW6Q1/my_pole_zero.m
+++ MATLABECE2311/HW6Q1/my_pole_zero.m
@@ -1,12 +1,13 @@
 function my_pole_zero()
-N = [1 0 -16];
+%ECE 2311 HW 6 Problem 1
+N = [1 0 16];
 D = [1 8 22 20];
 n = roots(N);
 d = roots(D);
 plot(n, 'o')
 hold on
 plot(d, 'x')
-axis nd
+axis auto
 xlabel ("Real Part"); ylabel("Imaginary Part");
 grid on
 end
